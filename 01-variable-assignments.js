c4807617093bba73c9a06aad2283af5fa4d6d6ea
Riddhivj234 <riddhivj234@gmail.com>
2024-10-25 15:30:45 +0530

    01-variable-assignment
    01-variable-assignment

---
+++ 01-variable-assignments.js
@@ -0,0 +1,16 @@
+
+var city = "Arvi";
+console.log("City: " ,city);
+
+var bestfriend = "Shruti";
+console.log("Bestfriend: ",bestfriend);
+
+var countryName = "India";
+console.log("Country: ",countryName);
+
+var schoolName = "Vidyaniketan English High School";
+console.log("School: ",schoolName);
+
+var college = "K L Amravati";
+console.log("College: ",college);
+
---
+++ 02-variable-assignment.js
@@ -0,0 +1,62 @@
+
+var fullName;
+console.log("Full Name: ",fullName);
+ 
+fullName = "Riddhi Joshi";
+console.log("First and Last Name: ",fullName);
+
+fullName = "Riddhi";
+console.log("First Name: ",fullName);
+
+fullName = "Vijay";
+console.log("Middle Name: ",fullName);
+
+fullName = "Joshi";
+console.log("Last Name: ",fullName);
+
+ var myLove = "I Love only JS";
+ console.log("Language: ",myLove);
+
+ var firstName = "Riddhi";
+ console.log("First Name: ",firstName);
+
+ var lastName = "Joshi";
+ console.log("Last Name: ",lastName);
+  
+ var age = "21";
+ console.log("Age: ",age);
+
+ var address = "Ganpati Ward Arvi";
+ console.log("Address: ",address);
+
+ var pincode = "442201"
+ console.log("Pincode: ",pincode);
+
+ var gender = "Female"
+ console.log("Gender: ",gender);
+
+ var isMarried = "False"
+ console.log("Is Married: ",isMarried);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
