185c42e19d9afead48b969753b4a67d507d8f3b9
Riddhivj234 <riddhivj234@gmail.com>
2024-10-26 20:02:05 +0530

    03-stringBasicsAssig01.js
    03-stringBasicsAssig01.js

---
+++ 03-stringBasicsAssig01.js
@@ -0,0 +1,33 @@
+
+function stringbasics(){
+    var dreamCompany = "Microsoft"
+    console.log(`My dream company is : ${dreamCompany}`)
+
+    console.log("======================================================");
+    
+   
+var hobboieOne = "Reading";
+var hobbieTwo = "Journaling";
+var hobbieThree = "Walking";
+
+console.log(`My hobbies are: ${hobboieOne}, ${hobbieTwo}, ${hobbieThree}`);
+
+console.log("============================================================");
+
+var result = hobboieOne.length + hobbieTwo.length + hobbieThree.length
+console.log(`Total number of characters: ${result}`);
+
+console.log("==============================================================");
+
+
+
+
+    
+}
+
+stringbasics();
+
+
+
+
+
