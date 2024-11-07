73492d6119a802a145ee01756bf4f2ba502177a7
Riddhivj234 <riddhivj234@gmail.com>
2024-11-07 20:18:51 +0530

    commit-06-for-loop-assignment01.js
    commit-06-for-loop-assignment01.js

---
+++ 06-for-loop-assignment01.js
@@ -0,0 +1,44 @@
+console.log("==========Numbers from 5 to 15 by incrementing by 1===========");
+
+for (var index = 5 ; index <= 15 ; index++) {
+    
+    console.log(index);
+    
+}
+
+console.log("====Numbers from 50 to 40 by decrementing by 1=========");
+
+for (var index = 50 ; index >= 40 ; index--) {
+    console.log(index);
+    
+}
+
+console.log("====First 15 odd numbers=======");
+for (var index = 1; index < 30 ; index = index+2) {
+    console.log(index);
+    
+}
+
+console.log("====First 10 even numbers=======");
+for (var index = 2; index <= 20 ; index = index+2) {
+    console.log(index);
+    
+}
+
+console.log("===========Table of 5===============");
+for (var index = 5; index <= 50 ; index = index+5) {
+    console.log(index);
+    
+}
+
+console.log("===========Table of 10===============");
+for (var index = 10; index <= 100 ; index = index+10) {
+    console.log(index);
+    
+}
+
+console.log("===========Table of 10 in reverse order===============");
+for (var index = 100; index >= 10 ; index = index-10) {
+    console.log(index);
+    
+}
