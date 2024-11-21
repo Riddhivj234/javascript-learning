9eb2ae2f7d81dea387fa9351d3e15097a29765e4
Riddhivj234 <riddhivj234@gmail.com>
2024-11-21 22:40:22 +0530

    commit-22-filterAssign.js
    commit-22-filterAssign.js

---
+++ 22-filterAssign.js
@@ -0,0 +1,35 @@
+
+
+
+const arrayNumbers = [20, 11, 40, 25, 37, 49, 9, 90, 60, 2, 19]
+console.log(`Given array ${arrayNumbers}`);
+const arrayN = arrayNumbers.filter( (element) => {
+    return element>50;
+} );
+console.log("==========Numbers greater than 50============");
+console.log(arrayN);
+
+console.log("===========All even numbers in the array============");
+const newArray = arrayNumbers.filter( (element) =>{
+    return element%2==0
+})
+console.log(newArray);
+
+console.log("=========All Odd numbers in the array======================");
+const oddNumbers = arrayNumbers.filter( (element) =>{
+    return element%2
+})
+console.log(oddNumbers);
+
+console.log("=========All numbers which are multiple of 5  in the array======================");
+const multipleArray = arrayNumbers.filter( (element) =>{
+    return element%5==0
+})
+console.log(multipleArray);
+
+console.log("=========All numbers which are between 20 to 50 in the array======================");
+const arrayNew = [20, 11, 40, 25, 37, 49, 9, 90, 60, 2, 19]
+const arrayNum = arrayNew.filter( (element) => {
+    return element>20& element<50;
+} );
+console.log(arrayNum);
